bc2649216a07d2966711cdd3a934b8690c0d3733
Catch-XXII <cgultekink@gmail.com>
2020-05-16 19:21:31 +0300

    Add files via upload

--- Kontakus/AppDelegate.swift
+++ Kontakus/AppDelegate.swift
@@ -19,9 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         UISearchBar.appearance().tintColor = .kontakBlue
         UINavigationBar.appearance().tintColor = .kontakBlue
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        AppDelegate.launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
 
-        if launchedBefore {
+        if AppDelegate.launchedBefore {
             print("Not first launch.")
         } else {
             print("First launch")
@@ -32,24 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
